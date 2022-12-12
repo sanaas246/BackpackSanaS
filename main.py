b73735955a49745381b82958f8006f0defd6022e
sanaas246 <112659421+sanaas246@users.noreply.github.com>
2022-12-12 14:43:25 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+class Backpack: 
+    def __init__(self, color, size, items, open):
+        self.color = color
+        self.size = size 
+        self.items = []
+        self.open = False
+
+    def openBag(self):
+        self.open = True
+        print("Backpack is open")
+
+    def closeBag(self):
+        self.open = False
+        print("Backpack is closed")
+
+    def putin(self,item):
+        if self.open == True:
+            if item in self.items:
+                self.items.append(item)
+                print(f"These items are in your bag: {self.items}")
+        else:
+            print("Backpack not open")
+
+    def takeout(self,item):
+        if self.open == True:
+            if item in self.items:
+                self.items.pop(item)
+                print(f"These items are in your bag: {self.items}")
+        else:
+            print("Backpack not open")
